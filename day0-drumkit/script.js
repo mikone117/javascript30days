f04b17bc99bb03d552055e628f59efc4cce52640
Miguel Medina <miguelmedinarodriguez@gmail.com>
2018-03-03 18:37:58 +0000

    Tiny drum kit playable with your keyboard

---
+++ day0-drumkit/script.js
@@ -0,0 +1,24 @@
+function playDrum(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    if (!audio) {
+        return //Stop function from running
+    }
+
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
+    audio.currentTime = 0
+    audio.play()
+
+    key.classList.add('playing')
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') {
+        return
+    }
+    this.classList.remove('playing')
+}
+window.addEventListener('keydown', playDrum)
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
