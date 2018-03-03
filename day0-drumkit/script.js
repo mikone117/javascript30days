ce789a49f641097b2bae2eb0f3e0b6791845fd59
Miguel Medina <miguelmedinarodriguez@gmail.com>
2018-03-03 19:48:15 +0000

    Updated to work with clicks

--- day0-drumkit/script.js
+++ day0-drumkit/script.js
@@ -1,15 +1,26 @@
-function playDrum(e) {
+function playDrumKey(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     if (!audio) {
         return //Stop function from running
     }
 
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-
     audio.currentTime = 0
     audio.play()
-
     key.classList.add('playing')
+}
+
+function playDrumClick(e) {
+    let element = e.target
+    if (element.className !== 'key') {
+        element = element.parentNode
+    }
+    let dataKey = element.getAttribute("data-key")
+
+    const audio = document.querySelector(`audio[data-key="${dataKey}"]`)
+    audio.currentTime = 0
+    audio.play()
+    element.classList.add('playing')
 }
 
 function removeTransition(e) {
@@ -18,7 +29,9 @@ function removeTransition(e) {
     }
     this.classList.remove('playing')
 }
-window.addEventListener('keydown', playDrum)
+
+window.addEventListener('keydown', playDrumKey)
 
 const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('click', playDrumClick))
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
